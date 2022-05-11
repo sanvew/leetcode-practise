f2fc25f8f4ac85f8f67e38ca288fe6c778ecf249
sanvew <sanvew@protonmail.com>
2022-05-12 00:53:55 +0400

    Updated run script

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 TIMEFORMAT='%Rs'
-file_name=$(basename bin/$1)
-mkdir -p bin && \
-    time g++ -Wfatal-errors -o bin/$file_name $1.cpp && \
-    ./bin/$file_name
+file_name=$(basename .bin/$1)
+mkdir -p .bin && \
+    time g++ -Wfatal-errors -o .bin/$file_name $1.cpp && \
+    ./.bin/$file_name
